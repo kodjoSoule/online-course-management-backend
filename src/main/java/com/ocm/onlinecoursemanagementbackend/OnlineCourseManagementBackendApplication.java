6f613d4b717cf4e5632ae201e0435e667dc3618b
Souleymane KODJO <76671691+kodjoSoule@users.noreply.github.com>
2024-06-18 23:45:04 +0000

    First commit.

---
+++ src/main/java/com/ocm/onlinecoursemanagementbackend/OnlineCourseManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ocm.onlinecoursemanagementbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineCourseManagementBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineCourseManagementBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ocm/onlinecoursemanagementbackend/OnlineCourseManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ocm.onlinecoursemanagementbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCourseManagementBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
